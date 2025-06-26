f7ba36ba9f844239b1ac21f2ba238bad4def7a3d
unknown <akashgupta00024@gmail.com>
2025-06-27 00:19:28 +0530

    QS4 Pending

---
+++ DataStructures/src/Stack/Question4NextGreaterElement.java
@@ -0,0 +1,21 @@
+package Stack;
+
+import java.util.Arrays;
+
+public class Question4NextGreaterElement {
+
+	public static int[] nextGreaterElements(int[] nums)
+	{
+		
+		
+		int arr[]=new int[nums.length];
+		return arr;
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
